cada5c16b799d9a6e53c88137caed26f17facc24
Kosmo <The_Kosmo@outlook.com>
2023-02-19 16:07:16 +0500

    3готов

---
+++ Source3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+#include <time.h>
+#include <iomanip>
+
+#pragma warning(disable: 4996)
+
+int  main()
+{
+	//Блок переменных
+	std::time_t timer = std::time(nullptr);
+	std::tm temp = *std::localtime(&timer);
+	
+	//Блок инициализации конечного времени
+	std::cout << "Timer\nInput time: (Min Sec)\n";
+	std::cin >> std::get_time(&temp, "%M %S");
+
+	timer = std::time(nullptr);
+	std::tm end_timer = *std::localtime(&timer);
+	end_timer.tm_min += temp.tm_min;
+	end_timer.tm_sec += temp.tm_sec;
+
+	
+	while (true)
+	{
+		if (std::time(nullptr) == std::mktime(&end_timer)) break;
+	}
+	std::cout << "Ding! Ding! Ding!";
+	return 0;
+}
